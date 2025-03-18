c3915cddda1465251f94f2cc76de0e3e464d8d69
HimanshuKumar2971 <himanshukumar290071@gmail.com>
2025-03-18 11:23:47 +0530

    addedd some codes inside in main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+        Product p=new Product();
+        p.product(2,4);
+        p.product(3,4,5);
+        p.product(23.344,45.23);
+
+    }
+
 }
